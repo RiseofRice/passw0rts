346460aea1450e5afd76a2eac79a4952e38c22f3
RiseofRice <xthekay@gmail.com>
2025-11-04 15:47:36 +0100

    Bump version from 0.1.0 to 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="passw0rts",
-    version="0.1.0",
+    version="0.1.5",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     package_data={
